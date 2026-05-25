6488a954dcb1f74b4c91ae7fd0dd3ada7646ba97
kael <theo.viti2@outlook.fr>
2026-05-25 15:02:54 +0200

    feat: UserService CRUD added

--- user-service/src/main/java/com/fitnetwork/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/fitnetwork/user_service/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 ///TODO
 /// CRUD Service
 /// Controller
+/// Validation
 /// Entity SQL annotations
 /// DTO admin?
 @SpringBootApplication
